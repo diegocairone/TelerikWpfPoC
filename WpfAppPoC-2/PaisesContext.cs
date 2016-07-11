4bec0352105b62fa998297c85f1fa284ff2d78c4
diegocairone <diegocairone@gmail.com>
2016-07-11 08:42:01 -0300

    Initializing a proof of concept for Telerik WPF

---
+++ WpfAppPoC-2/PaisesContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppPoC_2
+{
+    class PaisesContext : Servicios.SDLPoC
+    {
+        public PaisesContext() : base(new Uri(uriString: "http://localhost:8080/odata/PaisService.svc/"))
+        {
+
+        }
+    }
+}
