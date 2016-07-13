3d0f560ed55d2efc0d8b8f7b12ec638518bdc913
diegocairone <diegocairone@gmail.com>
2016-07-13 10:32:03 -0300

    Actualizacion soporte Telerik

--- WpfAppPoC-2/WinPaises.xaml.cs
+++ WpfAppPoC-2/WinPaises.xaml.cs
@@ -19,34 +19,98 @@ namespace WpfAppPoC_2
     /// </summary>
     public partial class WinPaises : Window
     {
+        private Boolean hayModificaciones = false;
 
         public WinPaises()
         {
             InitializeComponent();
         }
-
-        private void HabilitarCmds(Boolean estaEditando)
-        {
-            radBtnNuevo.IsEnabled = !estaEditando;
-            radBtnBorrar.IsEnabled = !estaEditando;
-            radBtnGuardar.IsEnabled = !estaEditando;
-            radBtnCancelar.IsEnabled = !estaEditando;
-        }
-
+        
         private void RadGridView_BeginningEdit(object sender, Telerik.Windows.Controls.GridViewBeginningEditRoutedEventArgs e)
         {
-            HabilitarCmds(estaEditando: true);
-        }
+            this.hayModificaciones = true;
 
-        private void RadGridPaises_RowEditEnded(object sender, Telerik.Windows.Controls.GridViewRowEditEndedEventArgs e)
-        {
-            HabilitarCmds(estaEditando: false);
+            this.radBtnGuardar.IsEnabled = true;
+            this.radBtnCancelar.IsEnabled = true;
+
+            BarraEstadoItem.Content = "DATOS MODIFICADOS !!";
         }
 
         private void RadBtnGuardarClick(object sender, RoutedEventArgs e)
         {
             paisesDataSource.SubmitChanges();
+            hayModificaciones = false;
         }
-        
+
+        private void RadBtnCancelarClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void RadGridPaises_DataLoading(object sender, Telerik.Windows.Controls.GridView.GridViewDataLoadingEventArgs e)
+        {
+            this.hayModificaciones = false;
+
+            this.radBtnNuevo.IsEnabled = false;
+            this.radBtnBorrar.IsEnabled = false;
+            this.radBtnGuardar.IsEnabled = false;
+            this.radBtnCancelar.IsEnabled = false;
+
+            BarraEstadoItem.Content = "RECIBIENDO DATOS ...";
+            System.Diagnostics.Debug.WriteLine(message: "Recibiendo..");
+        }
+
+        private void RadGridPaises_DataLoaded(object sender, EventArgs e)
+        {
+            this.hayModificaciones = false;
+
+            this.radBtnNuevo.IsEnabled = true;
+            this.radBtnBorrar.IsEnabled = false;
+            this.radBtnGuardar.IsEnabled = false;
+            this.radBtnCancelar.IsEnabled = false;
+
+            BarraEstadoItem.Content = "LISTO";
+            System.Diagnostics.Debug.WriteLine(message: "Listo");
+        }
+
+        private void RadGridPaises_SelectionChanged(object sender, Telerik.Windows.Controls.SelectionChangeEventArgs e)
+        {
+            this.radBtnBorrar.IsEnabled = RadGridPaises.SelectedItems.Count() != 0;
+            System.Diagnostics.Debug.WriteLine(format: "Filas seleccionadas {0}", args: new object[] { e.AddedItems.Count() });
+        }
+
+        private void RadBtnNuevoClick(object sender, RoutedEventArgs e)
+        {
+            RadGridPaises.BeginInsert();
+        }
+
+        private void RadBtnBorrarClick(object sender, RoutedEventArgs e)
+        {
+            if(RadGridPaises.SelectedItems.Count == 0)
+            {
+                return;
+            }
+
+            var itemsToRemove = new System.Collections.ObjectModel.ObservableCollection<object>();
+
+            foreach (var item in RadGridPaises.SelectedItems)
+            {
+                itemsToRemove.Add(item);
+            }
+
+            foreach (var item in itemsToRemove)
+            {
+                (RadGridPaises.ItemsSource as Telerik.Windows.Data.DataItemCollection).Remove(item);
+            }
+
+            paisesDataSource.SubmitChanges();
+            hayModificaciones = false;
+        }
+
+        private void RadGridPaises_AddingNewDataItem(object sender, Telerik.Windows.Controls.GridView.GridViewAddingNewEventArgs e)
+        {
+            var grid = e.OwnerGridViewItemsControl;
+            grid.CurrentColumn = grid.Columns[0];
+        }
     }
 }
