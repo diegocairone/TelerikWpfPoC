d5781aec8545ce7c2d7294931de304fd0dc00e0d
diegocairone <diegocairone@gmail.com>
2016-08-04 11:10:30 -0300

    Adding untracked file

---
+++ WpfAppPoC-2/ServiceContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace WpfAppPoC_2
+{
+    class ServiceContext : Servicios.SDLPoC
+    {
+        public ServiceContext() : base(new Uri(Properties.Settings.Default.RestServiceUri))
+        {
+            this.SendingRequest2 += (sender, eventArgs) =>
+            {
+                var nc = new NetworkCredential(userName: "admin", password: "qwerty123");
+
+                String token = nc.UserName + ":" + nc.Password;
+
+                String encoded = System
+                    .Convert
+                    .ToBase64String(System.Text.Encoding.GetEncoding(name: "ISO-8859-1")
+                    .GetBytes(token));
+
+                eventArgs.RequestMessage.SetHeader(headerName: "Authorization", headerValue: "Basic " + encoded);
+            };
+        }
+        
+    }   
+}
